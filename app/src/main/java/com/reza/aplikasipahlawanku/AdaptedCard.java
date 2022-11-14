7b227f66f785b30f75739b92b998afeefa5a4812
restianreza <114054889+restianreza@users.noreply.github.com>
2022-11-14 11:12:29 +0700

    update adapted Card

---
+++ app/src/main/java/com/reza/aplikasipahlawanku/AdaptedCard.java
@@ -0,0 +1,24 @@
+package com.reza.aplikasipahlawanku;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdaptedCard {
+
+    public class ClassViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivFoto;
+        TextView tvNama, tvTentang;
+
+
+        public ClassViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivFoto = itemView.findViewById(R.id.iv_foto);
+            tvNama = itemView.findViewById(R.id.tv_nama);
+            tvTentang = itemView.findViewById(R.id.tv_tentang);
+        }
+    }
+}
