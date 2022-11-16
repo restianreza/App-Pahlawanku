ff9f50e5c863ee3a9da7295c8409d4d92073fd6a
rezarestian <restianreza30@gmail.com>
2022-11-16 14:17:10 +0700

    update

--- app/src/main/java/com/reza/aplikasipahlawanku/AdaptedCard.java
+++ app/src/main/java/com/reza/aplikasipahlawanku/AdaptedCard.java
@@ -1,18 +1,44 @@
 package com.reza.aplikasipahlawanku;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdaptedCard {
+import java.util.ArrayList;
+
+public class AdaptedCard extends RecyclerView.Adapter<AdaptedCard.ClassViewHolder>{
+    private ArrayList<ModelPahlawan> dataPahlawan;
+
+    public AdaptedCard(ArrayList<ModelPahlawan> dataPahlawan) {
+        this.dataPahlawan = dataPahlawan;
+    }
+
+    @NonNull
+    @Override
+    public AdaptedCard.ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdaptedCard.ClassViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ClassViewHolder extends RecyclerView.ViewHolder {
         ImageView ivFoto;
         TextView tvNama, tvTentang;
-
 
         public ClassViewHolder(@NonNull View itemView) {
             super(itemView);
